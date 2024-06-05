c54a346472881b45e203abdff16993ba6d216384
Mostafijul Islam <mostafijul1000@gmail.com>
2024-06-06 02:11:35 +0600

    chore: bit of refactoring!

--- app/src/main/java/com/mostafij/androidweatherapp/ui/screen/Homepage.kt
+++ app/src/main/java/com/mostafij/androidweatherapp/ui/screen/Homepage.kt
@@ -2,6 +2,7 @@ package com.mostafij.androidweatherapp.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -18,18 +20,11 @@ import com.mostafij.androidweatherapp.ui.viewmodel.WeatherDataViewModel
 fun Homepage(modifier: Modifier = Modifier, viewModel: WeatherDataViewModel) {
     Column(
         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxWidth(),
     ) {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = modifier.fillMaxWidth(),
-        ) {
-            Text(text = "Hello Android!", modifier = modifier)
-        }
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = modifier.fillMaxWidth().padding(vertical = 12.dp),
-        ) {
+        Text(text = "Hello Android!", modifier = modifier)
+        Box(modifier = modifier.padding(vertical = 12.dp)) {
             TextButton(
                 onClick = { viewModel.getWeatherData("London") },
                 modifier = modifier.background(color = Color.Magenta),
